f6fc3a394e76c4deaf46be7d293dca0631cb25d8
Emad <emadahmadi1384@gmail.com>
2025-06-01 07:24:38 +0330

    Emad Ahmadi😁 Edith : Even Dead I'm The Hero. -Tony Stark

--- ForkForce.py
+++ ForkForce.py
@@ -57,3 +57,18 @@ def logarithm(n, base=10):
         current /= base
         result += 1
     return result
+
+#defined by Emad Ahmadi
+def factorial(n):
+    if not isinstance(n, int):
+        return "Error: Input must be an integer"
+    if n < 0:
+        return "Error: Input must be non-negative"
+    
+    if n == 0 or n == 1:
+        return 1
+    
+    result = 1
+    for i in range(2, n + 1):
+        result *= i
+    return result
